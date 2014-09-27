0e719233e0c4e88977795168eed5721d605fed29
Shawn Doucet <shawndoucet@gmail.com>
2014-09-27 08:46:06 -0500

    reflection hierarch tests

---
+++ Voodoo.Tests/Voodoo/ReflectionHierarchyTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Voodoo.Tests.Voodoo
+{
+    public abstract class TestBaseClass<T>
+        where T : class, new()
+    {
+    }
+
+    public class MessageClass
+    {
+    }
+
+    public class TestSubClass : TestBaseClass<MessageClass>
+    {
+    }
+
+    [TestClass]
+    public class ReflectionHierarchyTests
+    {
+        [TestMethod]
+        public void ProveSubClassInheritsFromBase()
+        {
+            var baseType =
+                typeof (MessageClass).Assembly.GetTypes().First(c => c.Name.StartsWith("TestBaseClass"));
+            var subType = new TestSubClass().GetType();
+
+            //Assert.AreEqual failed. Expected:<Voodoo.Tests.Voodoo.TestBaseClass`1[T]>. Actual:<Voodoo.Tests.Voodoo.TestBaseClass`1[Voodoo.Tests.Voodoo.MessageClass]>. 
+
+            Assert.AreNotEqual(baseType, subType.BaseType);
+        }
+    }
+}
---
+++ Voodoo/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Voodoo
+{
+    public class Constants
+    {
+        public struct SortDirection
+        {
+            public static readonly string Descending = "DESC";
+            public static readonly string Ascending = "ASC";
+        }
+    }
+}
