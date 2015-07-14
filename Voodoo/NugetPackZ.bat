75c8484f2c95a16b12d02baccc1c8c3ad1b727e6
Shawn Doucet <shawndoucet@azuresoft.net>
2015-07-14 08:58:59 -0500

    build and deployment changes

--- Voodoo/NugetPackZ.bat
+++ Voodoo/NugetPackZ.bat
@@ -4,7 +4,7 @@ msbuild Voodoo.csproj /t:Build /p:Configuration="Release net45"
 msbuild Voodoo.csproj /t:Build /p:Configuration="Release net451"
 msbuild Voodoo.csproj /t:Build /p:Configuration="Release net452"
 msbuild Voodoo.csproj /t:Build /p:Configuration="Release net46"
+msbuild Voodoo.csproj /t:Build /p:Configuration="Release PCL"
 
-C:\Users\Shawn\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta4\bin\dnx.exe --appbase "z:\Dropbox\Lib\Projects\voodoo\Voodoo" "C:\Users\Shawn\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta4\bin\lib\Microsoft.Framework.PackageManager\Microsoft.Framework.PackageManager.dll" pack "z:\Dropbox\Lib\Projects\voodoo\Voodoo" --configuration Release --out "bin"
-
+C:\Users\Shawn\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta5\bin\dnx.exe --appbase "z:\Dropbox\Lib\Projects\voodoo\Voodoo" "C:\Users\Shawn\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta5\bin\lib\Microsoft.Framework.PackageManager\Microsoft.Framework.PackageManager.dll" pack "z:\Dropbox\Lib\Projects\voodoo\Voodoo" --configuration Release --out "bin"
 nuget pack voodoo.nuspec
