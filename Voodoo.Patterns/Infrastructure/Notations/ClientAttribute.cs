3d0c3984ec74b50fb7f41b9afd99091393cf5c44
Shawn Doucet <shawndoucet@azuresoft.net>
2016-04-25 06:16:02 -0500

    added MapsTo attribute

--- Voodoo.Patterns/Infrastructure/Notations/ClientAttribute.cs
+++ Voodoo.Patterns/Infrastructure/Notations/ClientAttribute.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Voodoo.Infrastructure.Notations
 {
+	/// <summary>
+	///Adds target type to generrated models file
+	/// </summary>
 	public class ClientAttribute: Attribute
 	{
 	}
